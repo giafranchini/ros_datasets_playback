8fa9f64c119daf6a303f573bddd80e30640092b7
giacomo franchini <giacomo.franchini@polito.it>
2024-01-08 13:41:33 +0000

    add roxy sensor fusion launch + config

--- launch/roxy_fuse_tracking.launch.py
+++ launch/roxy_fuse_tracking.launch.py
@@ -16,22 +16,22 @@ def generate_launch_description():
 
     this_pkg_name = "etna_s3li_playback"
     this_package_dir = get_package_share_directory(this_pkg_name)
+    config_file = 'roxy_fixed_lag_smoother.yaml'
     smoother_params = os.path.join(
-        this_package_dir, 'config', 'roxy_fixed_lag_smoother.yaml')
-    bag_relative_path = "2023-07-03_17-07-44____20_minutes_test_1/2023-07-03_17-07-44__20_minutes_test/2023-07-03_17-07-44_0.db3"
-
-    log_level = LaunchConfiguration('log_level')
+        this_package_dir, 'config', config_file)
+    bag_relative_path = "custom_rosbag2__1/custom_rosbag2__1_0.db3"
     declare_log_level = DeclareLaunchArgument(
         'log_level',
-        default_value='INFO'
+        default_value='DEBUG'
     )
 
     container_fuse = ComposableNodeContainer(
         name='container_fuse',
         namespace='',
         package='rclcpp_components',
-        executable='component_container_mt',
-        emulate_tty=True,
+        executable='component_container',
+        # emulate_tty=True,
+        # prefix=["gdbserver localhost:3000"],
         composable_node_descriptions=[
             ComposableNode(
                 package='fuse_optimizers',
@@ -59,38 +59,30 @@ def generate_launch_description():
         condition=IfCondition(LaunchConfiguration("visualize")),
     )
 
-    rl_odometry_recorder = Node(
-        package="odometry_recorder",
-        executable="odometry_recorder_node",
-        name="rl_odometry_recorder",
-        parameters=[
-            {"topic_name": "/localisation/odometry/filtered"},
-            {"output_file": "rl_odometry2d.csv"}
+    imu_rotation = Node(
+        package="imu_transformer",
+        executable="imu_transformer_node",
+        output="screen",
+        remappings=[
+            ("imu_in", "/localisation/imu/data"),
+            ("imu_out", "/localisation/imu/data_rotated"), 
         ],
-    )
-
-    fuse_odometry_recorder = Node(
-        package="odometry_recorder",
-        executable="odometry_recorder_node",
-        name="fuse_odometry_recorder",
         parameters=[
-            {"topic_name": "/fuse/odometry/filtered"},
-            {"output_file": "fuse_odometry2d.csv"}
+            "target_frame := base_link",
         ],
     )
 
     return LaunchDescription([
-        DeclareLaunchArgument("visualize", default_value="true"),
         SetParameter(name='use_sim_time', value=True),
+        DeclareLaunchArgument("visualize", default_value="true"),
         ExecuteProcess(
             cmd=['ros2', 'bag', 'play',
                  PathJoinSubstitution([this_package_dir, 'bags', bag_relative_path]),
-                 '--clock', '-l'],
-            output='screen'
+                 '--clock', '--loop'],
+            output='screen',
         ),
         rviz2,
         declare_log_level,
-        container_fuse,
-        # rl_odometry_recorder,
-        # fuse_odometry_recorder
+        # imu_rotation,
+        # container_fuse,
     ])
